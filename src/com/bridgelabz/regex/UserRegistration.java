04ab9d34c387dbf84e08d907f8c0cd686c216247
Mrk0p <sachinwele23@gmail.com>
2023-02-07 17:33:10 +0530

    Added:Email ID in user registration

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -8,6 +8,7 @@ public class UserRegistration {
 
     static Pattern firstName = Pattern.compile("^([A-Z]{1}+)([a-z]{2,}+)$");
     static Pattern lastName = Pattern.compile("^([A-Z]{1}+)([a-z]{2,}+)$");
+    static Pattern emailId = Pattern.compile("^([a-z\\.]+)([a-z]*)@([a-z]{2,}+)\\.([a-z\\.]{2,}+)([a-z]*)$");
 
     public static void main(String[] args) {
 
@@ -26,6 +27,16 @@ public class UserRegistration {
         System.out.println("Enter Last name :");
         Matcher matcher2 = lastName.matcher(scanner.next());
         if (matcher2.matches()){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+
+        System.out.println();
+        System.out.println("Enter Email name : ");
+        Matcher matcher3 = emailId.matcher(scanner.next());
+        if(matcher3.matches()){
             System.out.println(true);
         }
         else {
