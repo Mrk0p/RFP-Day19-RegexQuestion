c2c764343f50ff22b7c64eb9f8f621528b03d31d
Mrk0p <sachinwele23@gmail.com>
2023-02-07 17:29:19 +0530

    Added: Last Name in user registration

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     static Pattern firstName = Pattern.compile("^([A-Z]{1}+)([a-z]{2,}+)$");
+    static Pattern lastName = Pattern.compile("^([A-Z]{1}+)([a-z]{2,}+)$");
 
     public static void main(String[] args) {
 
@@ -15,6 +16,16 @@ public class UserRegistration {
         Matcher matcher1 = firstName.matcher(scanner.next());
 
         if (matcher1.matches()){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+
+        System.out.println();
+        System.out.println("Enter Last name :");
+        Matcher matcher2 = lastName.matcher(scanner.next());
+        if (matcher2.matches()){
             System.out.println(true);
         }
         else {
